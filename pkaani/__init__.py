b00293cbb710b871b626b2a3bc3a687c3f3f5fb9
skanda <sastrys1@gene.com>
2025-02-18 01:24:51 -0800

    exporting prep_pdb also

--- pkaani/__init__.py
+++ pkaani/__init__.py
@@ -10,3 +10,5 @@ __all__ = ['ani_descriptors','ase_io_proteindatabank_mod', 'pkaani','run','prep_
 __version__ = "0.1.0"
 
 from pkaani.pkaani import calculate_pka
+from pkaani.prep_pdb import prep_pdb
+
